983c96976c5b9d6c348c2335869849d0d890802f
Urata Sadiku <sadikuurata@gmail.com>
2021-12-14 14:29:06 +0100

    add some changes

--- Base_converter.py
+++ Base_converter.py
@@ -1,6 +1,7 @@
 #TODO: Rename the file with lowercase
 from tkinter import *
 from tkinter import messagebox # TODO: Remove this as it is imported
+from converter_functions import *
 
 root = Tk()
 
@@ -40,70 +41,6 @@ label_3.grid(row=2, column=2)
 
 
 # CONVERTING FUNCTIONS 
-# TODO: Put those function in another file and import them
-def bin_to_bin(b):
-    return (bin(int(str(b), 2)))
-
-
-def bin_to_dec(b):
-    return int(str(b), 2)
-
-
-def bin_to_oct(b):
-    return oct(int(str(b), 2))
-
-
-def bin_to_hex(b):
-    return hex(int(str(b), 2))
-
-
-def dec_to_bin(d):
-    return bin(int(d))
-
-
-def dec_to_dec(d):
-    return int(d)
-
-
-def dec_to_oct(d):
-    return oct(int(d))
-
-
-def dec_to_hex(d):
-    return hex(int(d))
-
-
-def oct_to_bin(o):
-    return bin(int(str(o), 8))
-
-
-def oct_to_dec(o):
-    return int(str(o), 8)
-
-
-def oct_to_oct(o):
-    return oct(int(str(o), 8))
-
-
-def oct_to_hex(o):
-    return hex(int(str(o), 8))
-
-
-def hex_to_bin(h):
-    return bin(int(str(h), 16))
-
-
-def hex_to_dec(h):
-    return int(str(h), 16)
-
-
-def hex_to_oct(h):
-    return oct(int(str(h), 16))
-
-
-def hex_to_hex(h):
-    return hex(int(str(h), 16))
-
 
 def convert():
     filename = "history.txt"
---
+++ converter_functions.py
@@ -0,0 +1,62 @@
+
+def bin_to_bin(b):
+    return (bin(int(str(b), 2)))
+
+
+def bin_to_dec(b):
+    return int(str(b), 2)
+
+
+def bin_to_oct(b):
+    return oct(int(str(b), 2))
+
+
+def bin_to_hex(b):
+    return hex(int(str(b), 2))
+
+
+def dec_to_bin(d):
+    return bin(int(d))
+
+
+def dec_to_dec(d):
+    return int(d)
+
+
+def dec_to_oct(d):
+    return oct(int(d))
+
+
+def dec_to_hex(d):
+    return hex(int(d))
+
+
+def oct_to_bin(o):
+    return bin(int(str(o), 8))
+
+
+def oct_to_dec(o):
+    return int(str(o), 8)
+
+
+def oct_to_oct(o):
+    return oct(int(str(o), 8))
+
+def oct_to_hex(o):
+    return hex(int(str(o), 8))
+
+
+def hex_to_bin(h):
+    return bin(int(str(h), 16))
+
+
+def hex_to_dec(h):
+    return int(str(h), 16)
+
+
+def hex_to_oct(h):
+    return oct(int(str(h), 16))
+
+
+def hex_to_hex(h):
+    return hex(int(str(h), 16))
